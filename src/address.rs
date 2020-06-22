3a5a25a358c5fc97e4869f390c887838911bb330
Ansho Rei <anshorei@zeroid.bit>
2020-06-22 22:47:26 +0200

    use little endian when packing ports

--- src/address.rs
+++ src/address.rs
@@ -1,9 +1,10 @@
 use crate::error::Error;
 use serde::{Deserialize, Serialize};
-use std::io::{Read, Write};
+use std::io::{Read, Write, Bytes};
 use std::net::{TcpStream, SocketAddr, ToSocketAddrs};
 use base64::{encode, decode};
 use koibumi_base32 as base32;
+use std::convert::TryInto;
 
 #[derive(Debug)]
 pub struct ParseError {
@@ -101,7 +102,7 @@ impl Address {
 	/// ```
 	/// use zeronet_protocol::Address;
 	///
-	/// let bytes = vec![127, 0, 0, 1, 16, 225];
+	/// let bytes = vec![127, 0, 0, 1, 225, 16];
 	/// let address = Address::unpack(&bytes).unwrap();
 	/// assert_eq!(address.to_string(), "127.0.0.1:4321".to_string());
 	/// ```
@@ -111,15 +112,15 @@ impl Address {
 			6 => {
 				let mut array = [0u8; 4];
 				array.copy_from_slice(&bytes[..4]);
-				Ok(Address::IPV4(array, bytes[4] as u16 * 256 + bytes[5] as u16))
+				Ok(Address::IPV4(array, u16::from_le_bytes(bytes[4..6].try_into().unwrap())))
 			},
 			18 => {
 				let mut array = [0u8; 16];
 				array.copy_from_slice(&bytes[..16]);
-				Ok(Address::IPV6(array, bytes[16] as u16 * 256 + bytes[17] as u16))
+				Ok(Address::IPV6(array, u16::from_le_bytes(bytes[16..18].try_into().unwrap())))
 			}
 			12 => {
-				let port = bytes[10] as u16 * 256 + bytes[11] as u16;
+				let port = u16::from_le_bytes(bytes[10..12].try_into().unwrap());
 				let mut array = [0u8; 10];
 				array.copy_from_slice(&bytes[..10]);
 				let address = base32::encode(&array)?;
@@ -137,13 +138,13 @@ impl Address {
 	/// let address = Address::parse("127.0.0.1:4321").unwrap();
 	/// let packed = address.pack();
 	///
-	/// assert_eq!(packed, [127, 0, 0, 1, 16, 225]);
+	/// assert_eq!(packed, [127, 0, 0, 1, 225, 16]);
 	///
 	/// let address = Address::parse("ytcnzluhaxidtbf4.onion:4321").unwrap();
 	/// let packed = address.pack();
 	/// let unpacked = Address::unpack(&packed).unwrap();
 	///
-	/// assert_eq!(packed, [196, 196, 220, 174, 135, 5, 208, 57, 132, 188, 16, 225]);
+	/// assert_eq!(packed, [196, 196, 220, 174, 135, 5, 208, 57, 132, 188, 225, 16]);
 	/// assert_eq!(unpacked.to_string(), "ytcnzluhaxidtbf4.onion:4321".to_string());
 	/// ```
 	/// TODO: test IPV6 and Onion
@@ -151,18 +152,18 @@ impl Address {
 		match self {
 			Address::IPV4(address, port) => {
 				let mut bytes = address.to_vec();
-				bytes.append(&mut port.to_be_bytes().to_vec());
+				bytes.append(&mut port.to_le_bytes().to_vec());
 				bytes
 			},
 			Address::IPV6(address, port) => {
 				let mut bytes = address.to_vec();
-				bytes.append(&mut port.to_be_bytes().to_vec());
+				bytes.append(&mut port.to_le_bytes().to_vec());
 				bytes
 			},
 			Address::OnionV2(address, port) => {
 				let address = address.to_lowercase();
 				let mut bytes = base32::decode(address).unwrap();
-				bytes.append(&mut port.to_be_bytes().to_vec());
+				bytes.append(&mut port.to_le_bytes().to_vec());
 				bytes
 			},
 			_ => vec![],
